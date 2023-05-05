2c2656aeb00d5b63f66a1f47eaf5405c37ab202e
giddy11 <edoghotugiddy@gmail.com>
2023-05-05 18:48:27 +0100

    working on pointer decay

--- chapter02/fgets.c
+++ chapter02/fgets.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
+void enter_food(char *food);
+
+
 
 int main()
 {
-    char food[5];
+    char food[2];
 
-    printf("Enter Favorite food: \n");
-    // fgets(food, sizeof(food), stdin);
-    gets(food);
-
+    enter_food(food);
+    printf("size: %d\n", sizeof(food));
     printf("Your food is sweet: %s\n", food);
 
-
     return(0);
+}
+
+void enter_food(char *food)
+{
+    printf("Enter Favorite food: \n");
+    printf("size in function: %d\n", sizeof(food));
+    fgets(food, sizeof(food), stdin);
+
 }
